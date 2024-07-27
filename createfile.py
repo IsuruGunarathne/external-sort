419d91874cb11029f6ce4848315b41d4a855d71d
Isuru Gunarathne <isuru623@gmail.com>
2024-07-27 12:25:14 +0530

    Fix typo

--- createfile.py
+++ createfile.py
@@ -1,18 +1,26 @@
 import random
 
 def create_file():
-    target_size = 256 * 1024 * 1024  # 256 MB in bytes
+    size = 256
+    target_size = size * 1024 * 1024  # 256 MB in bytes
     avg_line_size = 8  # Average size of each line (number + newline)
     num_lines = target_size // avg_line_size
+    additional_lines = 0
+
+    print('Creating file ' + str(size) + ' MB...')
+    print('Number of lines: ' + str(num_lines))
     
     with open('unsorted.txt', 'w') as file:
         for i in range(num_lines):
             file.write(str(random.randint(1, 1000000)) + '\n')
             
-    # Ensure the file is exactly 256 MB
+    # Ensure the file is 256 MB
     with open('unsorted.txt', 'a') as file:
         while file.tell() < target_size:
             file.write(str(random.randint(1, 1000000)) + '\n')
+            additional_lines += 1
+    
+    print('Additional lines added: ' + str(additional_lines))
 
 create_file()
 print('File created successfully!')
