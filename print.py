89131cb5afaa90c1a669e689b6e03ca00089a5bd
Isuru Gunarathne <isuru623@gmail.com>
2024-10-06 14:05:48 +0530

    Remove runtime files

--- print.py
+++
@@ -1,6 +0,0 @@
-file_name = 'unsorted1.txt'
-peak_memory = '12.17'
-time_taken = '9184.05'
-print("file: ", file_name)
-print("peak memory: ", peak_memory, 'MB')
-print('total time: ',time_taken, 'seconds')
