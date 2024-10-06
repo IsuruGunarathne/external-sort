bed4b1cc97fe598a0beadd4ba2ef9a68447cfab5
Isuru Gunarathne <isuru623@gmail.com>
2024-10-06 10:24:38 +0530

    Add script to print details

---
+++ print.py
@@ -0,0 +1,6 @@
+file_name = 'unsorted3.txt'
+peak_memory = '12.5825'
+time_taken = '696.48'
+print("file: ", file_name)
+print("peak memory: ", peak_memory, 'MB')
+print('total time: ',time_taken, 'seconds')
